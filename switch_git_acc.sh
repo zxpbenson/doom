5510b5f52585bdb65cc8b5a91d571c5bfd9eb286
zxpbenson <zxpbenson@gmail.com>
2025-03-14 14:27:41 +0800

    更新切换git账号脚本

--- switch_git_acc.sh
+++ switch_git_acc.sh
@@ -2,30 +2,49 @@
 
 # 检查是否提供了参数
 if [ $# -ne 1 ]; then
-    echo "使用方法: $0 <文件名>"
+    echo "使用方法: $0 <文件名 可选项 github,corp>"
     exit 1
 fi
 
 ACC=$1
-TARGET_DIR=`cd ~; pwd`  # 请替换为实际存放 .a 和 .b 文件的目录
+TARGET_DIR=`cd ~; pwd`
 
 echo "ACC=$ACC, TARGET_DIR=$TARGET_DIR"
 
-# 检查文件是否存在
-if [ ! -f "$TARGET_DIR/.gitconfig.$ACC" ]; then
+function delOldGitCfgLink() {
+  # 检查文件是否存在
+  if [ ! -f "$TARGET_DIR/.gitconfig.$ACC" ]; then
     echo "错误: $TARGET_DIR/.gitconfig.$ACC 不存在"
     exit 1
-fi
+  fi
 
-# 如果软链接已存在，先删除
-if [ -L "$TARGET_DIR/.gitconfig" ]; then
+  # 如果软链接已存在，先删除
+  if [ -L "$TARGET_DIR/.gitconfig" ]; then
     rm "$TARGET_DIR/.gitconfig"
     echo "已删除现有的软链接 $TARGET_DIR/.gitconfig"
-fi
+  fi
+}
+
+case "$1" in
+    github)
+        delOldGitCfgLink
+        echo "switch to github config"
+        ln -s "$TARGET_DIR/.gitconfig.$ACC" "$TARGET_DIR/.gitconfig"
+        ;;
+    corp)
+        delOldGitCfgLink
+        echo "switch to corp git config"
+        ln -s "$TARGET_DIR/.gitconfig.$ACC" "$TARGET_DIR/.gitconfig"
+        ;;
+    *)
+        echo "无效参数，请使用 github | corp"
+        exit 1
+        ;;
+esac
+
 
 # 创建软链接
-ln -s "$TARGET_DIR/.gitconfig.$ACC" "$TARGET_DIR/.gitconfig"
-echo "软链接 $TARGET_DIR/.gitconfig.$ACC -> $TARGET_DIR/.gitconfig 创建成功"
+
 
 echo "================ git config --list ==============>>>"
 git config --list
