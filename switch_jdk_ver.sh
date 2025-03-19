2f54bc71c39956d205d51fde76d9e6f2ec4d287d
zxpbenson <zxpbenson@gmail.com>
2025-03-19 11:21:44 +0800

    增加jdk版本

--- switch_jdk_ver.sh
+++ switch_jdk_ver.sh
@@ -2,7 +2,7 @@
 
 # 检查是否提供了参数
 if [ $# -ne 1 ]; then
-    echo "使用方法: $0 <jdk-version> (只有 8，21，23，24 可选)"
+    echo "使用方法: $0 <jdk-version> (只有 8，11，17，21，23，24 可选)"
     exit 1
 fi
 
@@ -25,6 +25,16 @@ case "$1" in
         echo "switch to jdk8"
         ln -s $TARGET_DIR/jdk1.8.0_261.jdk $TARGET_DIR/jdk
         ;;
+    11)
+        delOldJdkLink
+        echo "switch to jdk11"
+        ln -s $TARGET_DIR/jdk-11.0.26.jdk $TARGET_DIR/jdk
+        ;;
+    17)
+        delOldJdkLink
+        echo "switch to jdk17"
+        ln -s $TARGET_DIR/jdk-17.0.14.jdk $TARGET_DIR/jdk
+        ;;
     21)
         delOldJdkLink
         echo "switch to jdk21"
@@ -41,7 +51,7 @@ case "$1" in
         ln -s $TARGET_DIR/jdk-24.jdk $TARGET_DIR/jdk
         ;;
     *)
-        echo "无效参数，请使用 8 | 21 | 23 | 24"
+        echo "无效参数，请使用 8 | 11 | 17 | 21 | 23 | 24"
         exit 1
         ;;
 esac
